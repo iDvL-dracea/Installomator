ecc8c1010c5bc6c739b738ca9411f59a4385a763
iDvL-dracea <58770969+iDvL-dracea@users.noreply.github.com>
2024-11-20 21:52:35 +0000

    NodeJS label stopped working due to Node latest website now redirecting you to the actual latest version. Fixed now.

--- fragments/labels/nodejs.sh
+++ fragments/labels/nodejs.sh
@@ -1,8 +1,10 @@
 nodejs)
     name="nodejs"
     type="pkg"
-    appNewVersion=$(curl -s https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')
-    appCustomVersion(){/usr/local/bin/node -v}
-    downloadURL="https://nodejs.org/dist/latest/node-$(curl -s https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p').pkg"
+    latest_version=$(curl -sL -w %{url_effective} -o /dev/null https://nodejs.org/dist/latest/)
+    version_number=$(basename "$latest_version")
+    appNewVersion="${version_number}"
+    appCustomVersion() {/usr/local/bin/node -v 2>/dev/null || echo "Not installed"}
+    downloadURL="${latest_version}node-${appNewVersion}.pkg"
     expectedTeamID="HX7739G8FX"
     ;;
